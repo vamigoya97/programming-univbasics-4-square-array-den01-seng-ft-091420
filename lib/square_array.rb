5fd60fd66154f4d9f321b23fe150f1e4396d40a1
Victor Alberto Amigo <amigo.victor.97@gmail.com>
2020-07-07 16:46:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count = 0 
   new_array
   while count <= array.length - 1 do
-    new_array[count] = array[count]**2 
+    new_array[count] = (array[count])**2
     count += 1 
   end
   return new_array
